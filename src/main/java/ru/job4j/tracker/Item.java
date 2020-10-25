8325391efb7875783e04218ee7604138826bba86
sergey <s-krasssavin@mail.ru>
2020-10-26 00:38:17 +0300

    6. Date. Отображение даты. [#379576]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
+    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
+
+    public LocalDateTime getCreated() {
+        return this.created;
+    }
 
     public int getId() {
         return id;
