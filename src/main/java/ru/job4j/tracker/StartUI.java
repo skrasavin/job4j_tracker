208fc3b2659cf01038db56a88afe1cce901307b2
sergey <s-krasssavin@mail.ru>
2020-10-29 01:42:19 +0300

    5. Tracker - хранилище [#379595]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,26 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
-        System.out.println(item.toString());
+        Item item = new Item("Erste");
+        Item item2 = new Item("Zweite");
+        Item item3 = new Item("Dritte");
+        Item item4 = new Item("Erste");
+        Tracker track = new Tracker();
+        track.add(item);
+        track.add(item2);
+        track.add(item3);
+        track.add(item4);
+        Item[] itemTest = track.findAll();
+        for (Item it : itemTest) {
+            System.out.println(it.getName());
+        }
+        System.out.println();
+        Item[] itemTest2 = track.findByName("Erste");
+        for (Item it : itemTest2) {
+            System.out.println(it.getName());
+        }
+        //System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        //System.out.println(item.toString());
+
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -10,6 +12,28 @@ public class Tracker {
         items[size++] = item;
         return item;
     }
+    public Item[] findAll() {
+        int ind = 0;
+        for (Item item : items) {
+            if (item != null) {
+                ind++;
+            }
+        }
+        return Arrays.copyOf(items, ind);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int ind = 0;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                rsl[ind] = item;
+                ind++;
+            }
+        }
+        return Arrays.copyOf(rsl, ind);
+    }
+
 
     public Item findById(int id) {
         Item rsl = null;
