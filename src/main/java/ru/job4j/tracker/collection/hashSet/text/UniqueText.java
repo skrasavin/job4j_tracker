7e6f94f1869175b515f14905a540da371fbafbba
sergey <s-krasssavin@mail.ru>
2020-11-14 01:49:38 +0300

    2. Идентичные тексты [#379508]

---
+++ src/main/java/ru/job4j/tracker/collection/hashSet/text/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.collection.hashSet.text;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        Collections.addAll(check, origin);
+        for (String tex : text) {
+            rsl = check.contains(tex);
+            if (!rsl) {
+                break;
+            }
+        }
+
+        return rsl;
+    }
+}
