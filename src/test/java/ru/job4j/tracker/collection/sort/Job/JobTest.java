7a419e4654d197520ce650b6c3a15992cc5a9062
sergey <s-krasssavin@mail.ru>
2020-11-18 23:55:17 +0300

    2. Комбинированный компаратор [#379491]

---
+++ src/test/java/ru/job4j/tracker/collection/sort/Job/JobTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker.collection.sort.Job;
+
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndIncrease() {
+        Comparator<Job> cmpNamePriority = new SortByName().thenComparing(Job::compareTo);
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenCompatorByPriorityAndIncrease() {
+        Comparator<Job> cmpNamePriority = Job::compareTo;
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndDecrease() {
+        Comparator<Job> cmpNamePriority = new SortByName().thenComparing(Collections.reverseOrder());
+        int rsl = cmpNamePriority.compare(
+                new Job("Fix task", 2),
+                new Job("Fix task", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByPriorityAndDecrease() {
+        Comparator<Job> cmpNamePriority = Collections.reverseOrder();
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 1),
+                new Job("Fix bug", 2)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
