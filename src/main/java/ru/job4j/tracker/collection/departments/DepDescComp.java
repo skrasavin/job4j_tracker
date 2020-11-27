0f41f04bb137feb5dfb1407a60bf68d046df0586
sergey <s-krasssavin@mail.ru>
2020-11-28 01:33:02 +0300

    2. Отсортировать департаменты [#379484]

--- src/main/java/ru/job4j/tracker/collection/departments/DepDescComp.java
+++ src/main/java/ru/job4j/tracker/collection/departments/DepDescComp.java
@@ -3,19 +3,21 @@ package ru.job4j.tracker.collection.departments;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         String[] arrLeft = left.split("/");
         String[] arrRight = right.split("/");
-        int index = 0;
+
         int rsl;
-        rsl = arrRight[index].compareTo(arrLeft[index]);
+        rsl = arrRight[0].compareTo(arrLeft[0]);
+
         if (rsl != 0) {
-            return arrLeft[index].compareTo(arrRight[index]);
+            return rsl;
+        }else {
+            return arrLeft[arrLeft.length - 1].compareTo(arrRight[arrRight.length - 1]);
         }
-        return rsl;
+
     }
 }
