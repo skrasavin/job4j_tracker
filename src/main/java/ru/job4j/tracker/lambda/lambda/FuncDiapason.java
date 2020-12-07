819c1c14be094b21ad2bc23c61938dcd8cf66d18
sergey <s-krasssavin@mail.ru>
2020-12-08 02:40:15 +0300

    5. Подсчет функции в диапазоне. [#379313]

--- src/main/java/ru/job4j/tracker/lambda/lambda/FuncDiapason.java
+++ src/main/java/ru/job4j/tracker/lambda/lambda/FuncDiapason.java
@@ -1,12 +1,8 @@
 package ru.job4j.tracker.lambda.lambda;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.UnaryOperator;
 
 public class FuncDiapason {
 
