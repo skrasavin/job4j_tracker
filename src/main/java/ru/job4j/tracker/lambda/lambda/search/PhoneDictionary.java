4630ae780269ce281fb7f4a4da058b4a66ba53d6
sergey <s-krasssavin@mail.ru>
2020-12-09 01:50:58 +0300

    6. Функции высшего порядка [#379314]

---
+++ src/main/java/ru/job4j/tracker/lambda/lambda/search/PhoneDictionary.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.lambda.lambda.search;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getName().equals(key);
+            }
+        };
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
