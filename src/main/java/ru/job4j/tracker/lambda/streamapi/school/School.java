f407968bd53585fda9b519a08ff9e71f0dc744ab
sergey <s-krasssavin@mail.ru>
2020-12-15 02:58:56 +0300

    4. Преобразование List в Map. [#379331]

--- src/main/java/ru/job4j/tracker/lambda/streamapi/school/School.java
+++ src/main/java/ru/job4j/tracker/lambda/streamapi/school/School.java
@@ -2,13 +2,16 @@ package ru.job4j.tracker.lambda.streamapi.school;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+    public Map<String, Integer> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(
                 predict
-        ).collect(Collectors.toList());
+        ).collect(Collectors.toMap(Student::getSurname, Student::getScore));
     }
 }
