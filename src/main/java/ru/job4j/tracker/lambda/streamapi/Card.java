0f904238c1d301e859ff4afc65c87beb41fa153d
sergey <s-krasssavin@mail.ru>
2020-12-20 01:38:11 +0300

    8. Перенос методов в Stream API [#379335]

---
+++ src/main/java/ru/job4j/tracker/lambda/streamapi/Card.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.lambda.streamapi;
+
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+
+public static void main(String[] args) {
+    Stream.of(Suit.values())
+            .flatMap(value -> Stream.of(Value.values())
+            .map(suit -> value + " " + suit))
+            .forEach(System.out::println);
+}
+}
