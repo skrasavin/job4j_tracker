63c951b8703007edc39506c3ae7372cd5b1d2992
sergey <s-krasssavin@mail.ru>
2021-01-19 23:35:02 +0300

    0. Stream API улучшения [#379338]

---
+++ src/main/java/ru/job4j/tracker/lambda/laststreamapi/highschool/Student.java
@@ -0,0 +1,48 @@
+package ru.job4j.tracker.lambda.laststreamapi.highschool;
+
+import java.util.Objects;
+
+/**
+ * student
+ */
+public class    Student {
+
+    private int score;
+    private String surname;
+
+    public Student(String surname, int score) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score
+                && surname.equals(student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+
+}
--- src/main/java/ru/job4j/tracker/lambda/laststreamapi/highschool/StudentLevel.java
+++ src/main/java/ru/job4j/tracker/lambda/laststreamapi/highschool/StudentLevel.java
@@ -2,8 +2,8 @@ package ru.job4j.tracker.lambda.laststreamapi.highschool;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * studLevel
@@ -11,9 +11,10 @@ import java.util.stream.Collectors;
 public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
-                .filter(student -> student.getScore() > bound)
-                .sorted(Comparator.comparing(Student::getScore))
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
+
 }
